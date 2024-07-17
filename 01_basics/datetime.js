026afd91fded3708f3f2c1dce5bc6a0fe72c99c4
Ghosty <87269080+Prabhatsingh001@users.noreply.github.com>
2024-07-17 20:15:43 +0000

    fourth

---
+++ 01_basics/datetime.js
@@ -0,0 +1,10 @@
+let mydate = new Date();
+console.log(mydate.toString());
+console.log(mydate.getMonth());
+console.log(mydate.toJSON());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toLocaleString());
+console.log(`hi ${mydate}`);
+// shallow copies:- properties share same reference
+// deep copy :- do not share same reference
+
