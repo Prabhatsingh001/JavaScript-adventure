77f7d20658052345d0f984f615240f4cd6c5d32b
Ghosty <87269080+Prabhatsingh001@users.noreply.github.com>
2024-07-18 10:12:39 +0000

    rome

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -2,30 +2,35 @@
 // Object.create 
 
 //  object literals
-const mysym = Symbol("key1")
-const js_user = {
-    name: "hitesh",
-    age: 18,
-    [mysym]:"hello",
-    location: "bhopal",
-    email: "prabhat@gmail.com",
-    isLoggedIn: false,
-    lastLoggin:["monday","tuesday"]
-}
+// const mysym = Symbol("key1")
+// const js_user = {
+//     name: "hitesh",
+//     age: 18,
+//     [mysym]:"hello",
+//     location: "bhopal",
+//     email: "prabhat@gmail.com",
+//     isLoggedIn: false,
+//     lastLoggin:["monday","tuesday"]
+// }
 
-// console.log(js_user["email"]);
-// console.log(js_user.lastLoggin);
+// // console.log(js_user["email"]);
+// // console.log(js_user.lastLoggin);
 
-// symbol :- a datatype in js
-js_user.greeting = function(){
-    console.log("hello world");
-}
+// // symbol :- a datatype in js
+// js_user.greeting = function(){
+//     console.log("hello world");
+// }
 
-js_user.greetingTwo = function(){
-    console.log(`hello ${this.name}`);
-}
+// js_user.greetingTwo = function(){
+//     console.log(`hello ${this.name}`);
+// }
 
-console.log(js_user.greeting());
-console.log(js_user.greetingTwo());
-Object.freeze(js_user)
+// console.log(js_user.greeting());
+// console.log(js_user.greetingTwo());
+// Object.freeze(js_user)
 
+const obj1 = {1:'a',2:'b'}
+const obj2 = {3:'c',4:'d'}
+
+const obj3 = {...obj1,...obj2}
+console.log(obj3);
