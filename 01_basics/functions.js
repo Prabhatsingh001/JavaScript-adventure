ff26f83adc481e45a8ba3efe4159aa46941f7161
Ghosty <87269080+Prabhatsingh001@users.noreply.github.com>
2024-07-18 11:27:10 +0000

    hello

---
+++ 01_basics/functions.js
@@ -0,0 +1,17 @@
+function myname(){
+    console.log("hello");
+}
+
+// myname()
+
+function addTwonum(num1, num2){
+    console.log(num1+num2);
+}
+
+addTwonum(1,2);
+addTwonum();
+
+function handlobject(anybject){
+    
+}
+
