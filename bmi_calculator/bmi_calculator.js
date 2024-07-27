8b9d497f803947496b428560d9cba5b51786a826
Ghosty <87269080+Prabhatsingh001@users.noreply.github.com>
2024-07-27 15:44:15 +0000

    calc

--- bmi_calculator/bmi_calculator.js
+++ bmi_calculator/bmi_calculator.js
@@ -10,9 +10,13 @@ form.addEventListener("submit", function(event){
 
     if(height === '' || height<0 || isNaN(height)){
         result.innerHTML = `please give a valid height ${height}`;
-    }else if(weight === '' || weight<0 || isNaN(weight)){
-        result.innerHTML = `please give a valid height ${weight}`;
+    }
+    else if(weight === '' || weight<0 || isNaN(weight)){
+        result.innerHTML = `please give a valid weight ${weight}`;
     }else{
-        
+        const bmi = (weight/((height*height)/10000)).toFixed(2);
+        // show the result
+        result.innerHTML = `<span>${bmi}</span>`
+
     }
 })
