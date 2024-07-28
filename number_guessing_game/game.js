fe0245e1dee8c5ea2d9a3bd69a3264de6c53eebb
Ghosty <87269080+Prabhatsingh001@users.noreply.github.com>
2024-07-28 09:47:52 +0000

    Pending changes exported from your codespace

--- number_guessing_game/game.js
+++ number_guessing_game/game.js
@@ -23,19 +23,44 @@ if(playGame){
     })
 }
 function validateGuess(guess){
-    // 
+    if(guess==='' ||   isNaN(guess)){
+        alert('please enter a number');
+    }else if(guess<1){
+        alert('please enter a number greater than 1');
+    }else if(guess>100){
+        alert('please enter a number less than 100');
+    }else{
+        prevGuess.push(guess);
+        if(numGuess===11){
+            displayGuess(guess);
+            displayMsg(`game over. random number was ${randomNumber}`)
+            endGame();
+        }else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
 }
 
 function checkGuess(guess){
-    // 
+    if(guess === randomNumber){
+        displayMsg("you guesses right");
+    }else if(guess < randomNumber){
+        displayMsg("number is too low");
+    }else if(guess > randomNumber){
+        displayMsg("number id too high");
+    }
 }
 
 function displayGuess(guess){
-    // 
+    userInput.value = '';
+    userGuess.innerHTML += `${guess}`
+    numGuess++;
+    lastresult.innerHTML = `${11-numGuess}`
 }
 
 function displayMsg(msg){
-    // 
+    low_or_hi.innerHTML = `<h2>${msg}</h2>`
 }
 
 function newGame(){
